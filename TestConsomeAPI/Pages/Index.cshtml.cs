0829caa0794d62d73b845ffb8c7ef86668e2a71c
Mauricio Junior <mauricio.junior@gmail.com>
2022-04-22 21:08:53 -0500

    add project
    add project

---
+++ TestConsomeAPI/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using CallApi.Library;
+using CallApi.Library.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestConsomeAPI.Model;
+
+namespace TestConsomeAPI.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task OnGet()
+    {
+        CallApi.Library.Main _main = new Main();
+        var result = await _main.ExecuteCall<List<WeatherForecastModel>>("https://localhost:7036", 
+                                        "/WeatherForecast", 
+                                        CallType.GET,
+                                        null, 
+                                        null, 
+                                        null, 
+                                        null);
+
+        Response.WriteAsync(result.Count().ToString());
+        
+    }
+}
